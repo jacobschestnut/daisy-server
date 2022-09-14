6daf06c59e506a89b3eb02acf8e9675f8a242782
Jake Chestnut <jacobschestnut@gmail.com>
2022-09-14 14:31:29 -0500

    got update workign in back end

--- daisyapi/views/cocktail.py
+++ daisyapi/views/cocktail.py
@@ -3,9 +3,13 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from daisyapi.models import Cocktail
+from daisyapi.models import Cocktail, glass, ice, preparation
+from daisyapi.models import Glass, Ice, Preparation
+from daisyapi.models import ingredient
+from daisyapi.models.cocktail_ingredient import CocktailIngredient
 from daisyapi.models.mixologist import Mixologist
 from daisyapi.models.ingredient import Ingredient
+from daisyapi.models.unit import Unit
 from daisyapi.views.cocktail_ingredient import CocktailIngredientSerializer
 
 
@@ -61,6 +65,61 @@ class CocktailView(ViewSet):
                 cocktail_ingredient = cocktail_serializer.save(cocktail=new_cocktail,ingredient=ing_obj)
                 cocktail_res_serializer = CocktailIngredientSerializer(cocktail_ingredient)
         return Response(res_serializer.data, status=status.HTTP_201_CREATED)
+    
+    def update(self, request, pk):
+        """Handle PUT requests for a cocktail
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        ice = Ice.objects.get(pk=request.data['ice'])
+        glass = Glass.objects.get(pk=request.data['glass'])
+        preparation = Preparation.objects.get(pk=request.data['preparation'])
+        # ingredients = request.data.get("ingredients")
+        # if ingredients:
+        #     del request.data["ingredients"]
+        serializer = CreateCocktailSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        cocktail = Cocktail.objects.get(pk=pk)
+        cocktail.name = request.data["name"]
+        cocktail.description = request.data["description"]
+        cocktail.instructions = request.data["instructions"]
+        cocktail.img_url = request.data["img_url"]
+        cocktail.glass = glass
+        cocktail.ice = ice
+        cocktail.preparation = preparation
+        # if ingredients:
+        #     for cock_ing in ingredients:
+        #         print(cock_ing)
+        #         ing_obj = Ingredient.objects.get(pk=cock_ing['ingredient'])
+        #         cocktail = Cocktail.objects.get(pk=pk)
+        #         unit = Unit.objects.get(pk=cock_ing['unit'])
+        #         cock_ing["ingredient"] = ing_obj
+        #         cock_ing['cocktail'] = cocktail
+        #         cock_ing['unit'] = unit
+        #         cock_ing['amount'] = cock_ing['amount']
+        #         cock_ing.save()
+        #         ing_obj = CocktailIngredient.objects.get(pk=data_ingredient['ingredient'])
+        #         cocktail_serializer = CocktailIngredientSerializer(data=data_ingredient)
+        #         cocktail_serializer.is_valid(raise_exception=True)
+        #         cocktail_ingredient = cocktail_serializer.save(cocktail=cocktail,ingredient=ing_obj)
+        #         cocktail_res_serializer = CocktailIngredientSerializer(cocktail_ingredient)
+        # ingredients = CocktailIngredient.objects.get(pk=request.data["cocktail_ingredient"])
+        # cocktail.ingredients = ingredients
+        cocktail.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        cocktail = Cocktail.objects.get(pk=pk)
+        cocktail.delete()
+        try:
+            cocktail_ingredients = CocktailIngredient.objects.get(cocktail=pk)
+            if cocktail_ingredients:
+                for ingredient in cocktail_ingredients:
+                    ingredient.delete()
+        except:
+            return Response(None, status=status.HTTP_204_NO_CONTENT)
     
 class CocktailSerializer(serializers.ModelSerializer):
     """JSON serializer for game types
