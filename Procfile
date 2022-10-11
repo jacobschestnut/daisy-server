6b9e9644604914dfb30bb2480cbc08fd96751355
Jake Chestnut <jacobschestnut@gmail.com>
2022-10-11 11:32:28 -0500

    added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn daisy.wsgi --log-file -
--- daisy/settings.py
+++ daisy/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +142,9 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000'
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+django_on_heroku.settings(locals())
